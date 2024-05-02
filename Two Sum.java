b099ab116f442d6783525843f345a8fb150f440c
Anant Bhatnagar <anantbhatnagar74@gmail.com>
2024-05-02 20:22:04 +0530

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,31 +1,49 @@
 # Sum up to Target in Array
 
 // Two Sum Calculation using Brute Force Approach or Hash Table.
-// Return the indices of the array that sum up to target value.
+// Return the indices along with values from the array that sum up to target value.
 
-import java.util.*;
-public class TwoSum
+import java.util.Arrays;
+public class Two_Sum 
 {
-    public static void main(String args[])
+    public static int[] findTwoSum(int[] nums, int target) 
     {
-        int [] arr = {11,3,7,9,14,2};
-        int target =17;
-        int [] ans = new int [2];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i< arr.length; i++)
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) 
         {
-            int sno = target - arr[i];
-            if(map.containsKey(sno))
+            int sum = nums[left] + nums[right];
+            if (sum == target) 
             {
-                ans[0] = map.get(sno);
-                ans[1] = i;
-                break;
-          
+                return new int[]{left, right};
+            } 
+            else if (sum < target) 
+            {
+                left++;
+            } 
+            else 
+            {
+                right--;
             }
-            map.put(arr[i], i);
         }
-        System.out.println(ans[0] + " " + ans[1]);
+        return new int[0];
+    }
+    public static void main(String[] args) 
+    {
+        int[] nums = {2, 7, 11, 15};
+        int target = 17;
+        int[] result = findTwoSum(nums, target);
+        if (result.length == 2) 
+        {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+            System.out.println("Values: [" + nums[result[0]] + ", " + nums[result[1]] + "]");
+        } 
+        else 
+        {
+            System.out.println("No valid pair found.");
+        }
     }
 }
-
-Output : 1 4
+Output : Indices: [0, 3]
+         Values: [2, 15]
